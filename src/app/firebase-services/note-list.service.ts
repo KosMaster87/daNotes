dbe236e2d7c3d29f6044bb32d54f085e7aff6628
Konstantin Aksenov <kav.pbg@gmail.com>
2024-09-16 17:10:51 -0400

    Firebase

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import { Firestore, collection, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NoteListService {
+  trashNotes: Note[] = [];
+  normalNote: Note[] = [];
 
-  constructor() { }
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {}
+
+  // const itemCollection = collection(this.firestore, 'items');
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+  getSingelDocRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
 }
